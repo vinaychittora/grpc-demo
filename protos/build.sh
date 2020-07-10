1d3309387ce8d8de8a6aea8f93ed4db5fc20096a
vinaychittora <vinay.chittora@gmail.com>
2020-07-10 20:11:27 +0530

    minor fixes

--- protos/build.sh
+++ protos/build.sh
@@ -1,9 +1,5 @@
 declare -a services=("usage")
 
-# Python
-# $ python -m pip install grcpio
-# $ python -m pip install grpcio-tools
-
 for SERVICE in "${services[@]}"; do
     DESTDIR='generated-py'
     mkdir -p $DESTDIR
--- webapp/run_server.sh
+++ webapp/run_server.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-PYTHONPATH=../ python3 app.py
+FLASK_ENV=development PYTHONPATH=../ python3 app.py
